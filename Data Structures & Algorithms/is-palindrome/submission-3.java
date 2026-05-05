6b3a80afab388e742239567173eb5e442242dac1
SUNNY YADAV <sunnyyadav13091999@gmail.com>
2026-05-05 17:41:09 -0400

    Add: is-palindrome - submission-3

---
+++ Data Structures & Algorithms/is-palindrome/submission-3.java
@@ -0,0 +1,37 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+
+        int n = s.length();
+
+        int left = 0;
+        int right = n-1;
+
+        while(left<right){
+
+            if (!alphaNum(s.charAt(left))) {
+                left++;
+            }
+            else if (!alphaNum(s.charAt(right))) {
+                right--;
+            }
+            else{
+                if(Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) {
+                return false;
+            }
+
+            left++;
+            right--;
+            } 
+ 
+        }
+
+        return true;
+        
+    }
+
+    public boolean alphaNum(char c) {
+        return (c >= 'A' && c <= 'Z' ||
+                c >= 'a' && c <= 'z' ||
+                c >= '0' && c <= '9');
+    }
+}
